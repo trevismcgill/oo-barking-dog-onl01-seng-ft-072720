038e56989c571da6a3cc0fc2558e4b6b31cc35a7
Trevis McGill <trevismcgill25@gmail.com>
2020-08-02 21:35:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def name=(doge_name)
+    @this_doge_name = doge_name
+  end
+
+  def name
+    @this_doge_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
